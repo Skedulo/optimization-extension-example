8d968d4fe2e9c92ecbe0871342d8db5cab87662e
sabbey-skedulo <sam.abbey@skedulo.com>
2024-10-07 14:15:32 +0100

    initial commit

---
+++ src/routes.ts
@@ -0,0 +1,60 @@
+/**
+ * Describe the entry-point into the "skedulo-function" by updating the
+ * array returned by the `getRoutes` method with additional routes
+ */
+import * as pathToRegExp from "path-to-regexp";
+
+import { FunctionRoute } from "@skedulo/sdk-utilities";
+import {createOptimizationRoutes} from "@skedulo/optimization-manager-client";
+import {
+  TransformerInput,
+  TransformerOutput
+} from "@skedulo/optimization-manager-client/dist/transformers/scheduleTransformer";
+
+// tslint:disable-next-line:no-empty-interface
+interface RequestPayload {}
+
+export function getCompiledRoutes() {
+  return getRoutes().map((route) => {
+    const regex = pathToRegExp(route.path);
+
+    return {
+      regex,
+      method: route.method,
+      handler: route.handler,
+    };
+  });
+}
+
+const transformSchedule = async (plan: TransformerInput): Promise<TransformerOutput> => {
+  const allocationIds = plan.featureModel.allocations.map((allocation) => allocation.id);
+  // Create a single resource dependency for all allocations. This means that every allocation will be given to one resource.
+  const resourceDependencies = [{
+    allocationIds: allocationIds,
+  }]
+  return {
+    productData: plan.productData,
+    featureModel: {
+      ...plan.featureModel,
+      resourceDependencies,
+    },
+  };
+}
+
+function getRoutes(): FunctionRoute[] {
+  return [
+    {
+      method: "get",
+      path: "/ping",
+      handler: async (__, headers, method, path, skedContext) => {
+        const apiServer = skedContext.auth.baseUrl;
+
+        return {
+          status: 200,
+          body: { result: "pong", apiServer},
+        };
+      },
+    },
+      ...createOptimizationRoutes(transformSchedule)
+  ];
+}
